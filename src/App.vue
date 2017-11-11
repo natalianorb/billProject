d0629773757d05116e077c3b135c0de74cb1911a
Natalya Norboyeva <divonata@gmail.com>
2017-11-11 17:35:14 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Table/>
+  </div>
+</template>
+
+<script>
+import Table from './components/Table';
+
+export default {
+  name: 'app',
+  components: {
+    Table,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
